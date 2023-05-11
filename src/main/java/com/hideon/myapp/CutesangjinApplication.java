caa421ea1bf0b2d1db9a2189d6af574abceb2a41
Ye0ngjin <wkgek@naver.com>
2023-05-11 15:01:27 +0900

    스프링부트 테스트

---
+++ src/main/java/com/hideon/myapp/CutesangjinApplication.java
@@ -0,0 +1,13 @@
+package com.hideon.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutesangjinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutesangjinApplication.class, args);
+	}
+
+}
